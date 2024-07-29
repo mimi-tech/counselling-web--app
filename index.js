b82d5a5523e6611af6332842a0d8b0b92c219745
mimi-tech <onuohamiriam44@gmail.com>
2024-07-29 16:01:10 +0100

    Fixed issue

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 require('dotenv').config();
 const express = require ("express");
-const http = require("http");
 const bodyParser = require("body-parser");
 var cors = require('cors')
 const routes = require('./routes');
 
 const app = express();
-const server = http.createServer(app);
 app.use(cors());
 
 app.set('view engine','ejs');
@@ -17,8 +15,7 @@ app.use('/', routes);
 
 
 const PORT = process.env.PORT || 3002;
-server.listen(PORT, function(){
+app.listen(PORT, function(){
   console.log("server running at port 3001");
 });
 
-module.exports = app;
